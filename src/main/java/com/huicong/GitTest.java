3112b0e736525645e7ebc0ee9a061266d61b88ba
chiancc <chiancc@gmail.com>
2022-07-15 11:21:07 +0800

    first commit

---
+++ src/main/java/com/huicong/GitTest.java
@@ -0,0 +1,12 @@
+package com.huicong;
+
+/**
+ * @author Chiancc
+ * @version 1.0
+ * @date 2022/7/15 11:19
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
