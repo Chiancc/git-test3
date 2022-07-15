87546db5f8dcc3d5babef3443d3308e98b25be69
chiancc <chiancc@gmail.com>
2022-07-15 11:21:46 +0800

    second commit

--- src/main/java/com/huicong/GitTest.java
+++ src/main/java/com/huicong/GitTest.java
@@ -8,5 +8,6 @@ package com.huicong;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world2");
     }
 }
